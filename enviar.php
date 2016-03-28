848dbf47cd3bb3e3cf1e79314590f539e188bb01
komoshi <diana.man.91@gmail.com>
2016-03-28 19:51:59 -0300

    guardar variables en php

--- enviar.php
+++ enviar.php
@@ -1,13 +1,33 @@
 <h1>DATOS RECIBIDOS!</h1>
 <?php
-	echo "Recibido:<br>" . $_GET["nombre"] ."<br>";
+	echo "Recibido por GET:<br>";
 	var_dump($_GET);
+	echo "<p>Recibido por POST: <br>";
+	var_dump($_POST);
+
+	//echo $_GET["nombre"];
+	echo "<hr>";
+	$nombre = $_POST['nombre'];
+	echo $nombre;
+
 	/*
 	Hay dos maneras de enviar y recibir datos en PHP
 	$_POST
 	$_GET
 
 	var_dump() es una funcion para saber el contenido y el tipo de una variable
+	
+	----------------
+	HTTP envia datos cliente-servidor
+
+	GET:
+	 *envia los datos por la url
+	 *no puede enviar mas de 2048 caracteres
+	 *sólo envia los cuadros de texto que poseen name
+	POST 
+	 *no muestra los datos por la url
+	 *no tiene límite de caracteres
+	 *sólo envia los cuadros de texto que poseen name
 
 	*/
 ?>
--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 	<title></title>
 </head>
 <body>
-<form action="enviar.php">
+<form action="enviar.php" method="POST">
 	<input type="text" name="nombre"></input>
+	<input type="password" name="clave"></input>
 	<input type="submit" value="enviar_nombre"></input>
 </form>
 </body>
