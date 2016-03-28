9284bac95355df5a94a64e717fb27a7691c0208f
komoshi <diana.man.91@gmail.com>
2016-03-28 19:32:45 -0300

    primer get

---
+++ enviar.php
@@ -0,0 +1,13 @@
+<h1>DATOS RECIBIDOS!</h1>
+<?php
+	echo "Recibido:<br>" . $_GET["nombre"] ."<br>";
+	var_dump($_GET);
+	/*
+	Hay dos maneras de enviar y recibir datos en PHP
+	$_POST
+	$_GET
+
+	var_dump() es una funcion para saber el contenido y el tipo de una variable
+
+	*/
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="enviar.php">
+	<input type="text" name="nombre"></input>
+	<input type="submit" value="enviar_nombre"></input>
+</form>
+</body>
+</html>
